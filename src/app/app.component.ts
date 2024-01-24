81ce6f7e9a4fdd606ca01077c404d2b6f0d7287a
Sq1754 <Saqlain4517@gmail.com>
2024-01-24 12:44:05 +0530

    Banner issue Solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',

f2914618d9f40425ed07a6fbf4886970338240ea
Mayank Meghwal <mayankmeg207@gmail.com>
2024-05-28 14:28:24 +0530

    css

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { SignInPage } from './sign-in/sign-in.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+  },
+  { path: 'sign-in', component: SignInPage },
 ];
 
 @NgModule({
